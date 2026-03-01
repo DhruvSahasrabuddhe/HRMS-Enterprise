ac7508eb847d262c68aad751a87e7cc0ef51946a
shubhamnangare <shubhamnangare1111@gmail.com>
2026-03-01 13:42:37 +0530

    feat: set initial setup for project

---
+++ src/HRMS.Core/Entities/Base/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRMS.Core.Entities.Base
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public string? CreatedBy { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public string? UpdatedBy { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        [Timestamp]
+        public byte[]? RowVersion { get; set; }
+    }
+}
